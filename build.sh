188e386c411cdb785873cba7677e3e56864e6f54
SecureCRT <securecrt2008@gmail.com>
2012-08-29 23:55:08 +0800

    add build sh for HWA kernel

--- build.sh
+++ build.sh
@@ -1,53 +1,30 @@
 #!/bin/sh
-#This script made by Dorimanx and HIEROS.
+LOCAL_COMPILER=/home/securecrt/tools/gcc-linaro/bin/arm-eabi-
+KERNELBASEDIR=/ics/kernel/out
+rm arch/arm/boot/zImage
+make htcleo_defconfig
+make ARCH=arm CROSS_COMPILE=$LOCAL_COMPILER zImage -j8 && make ARCH=arm CROSS_COMPILE=$LOCAL_COMPILER modules -j8
 
-#GCC 4.5.2
-#COMPILER=/android-kernel/ICS/toolchain/bin/arm-none-eabi-
-
-#GCC 4.4.3
-COMPILER=/media/Source-Code/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-
-#GCC 4.4.5
-#COMPILER=/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi-
-
-#VERYOLDCOMPILER=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-
-
-#LINARO 12.4 GCC 4.7 (not working for now.
-#COMPILER=/android-kernel/ICS/linaro/bin/arm-eabi-
-
-rm -f arch/arm/boot/zImage
-rm -f /home/root/kernel/dorimanx-ics/output_gb/boot/zImage
-rm -f  /home/root/kernel/dorimanx-ics/output_gb/boot/boot.img
-rm -rf /home/root/kernel/dorimanx-ics/output_gb/system/lib/modules/*
-rm -rf /home/root/kernel/dorimanx-ics/output_gb/devs/*
-make ARCH=arm CROSS_COMPILE=$COMPILER zImage -j8
 if [ -f arch/arm/boot/zImage ]; then
-if [ ! -d /home/root/kernel/dorimanx-ics/output_gb ]; then
-mkdir -p /home/root/kernel/dorimanx-ics
-mkdir /home/root/kernel/dorimanx-ics/output_gb
-mkdir /home/root/kernel/dorimanx-ics/output_gb/boot
-mkdir /home/root/kernel/dorimanx-ics/output_gb/devs
-mkdir /home/root/kernel/dorimanx-ics/backup
-mkdir -p /home/root/kernel/dorimanx-ics/system/lib/modules
-fi
-cp /home/root/kernel/dorimanx-ics/output_gb/Kernel_Dorimanx-HWA-V*.zip /home/root/kernel/dorimanx-ics/backup
-rm -f /home/root/kernel/dorimanx-ics/output_gb/Kernel_Dorimanx-HWA-V*.zip
-cp arch/arm/boot/zImage /home/root/kernel/dorimanx-ics/output_gb/boot/zImage
-#mkbootimg --kernel /home/root/kernel/dorimanx-tytyng/output_gb/boot/zImage --ramdisk /home/root/kernel/dorimanx-tytyng/output_gb/boot/initrd.gz --cmdline "console=null" --base 0x11800000 -o /home/root/kernel/dorimanx-tytyng/output_gb/boot.img
-cp .config /home/root/kernel/dorimanx-ics/output_gb/devs/config
-cp .config arch/arm/configs/htcleo_defconfig
-cp .config .config.HWA
-cp arch/arm/configs/htcleo_defconfig /home/root/kernel/dorimanx-ics/output_gb/devs/htcleo_defconfig
-rm -rf /home/root/kernel/dorimanx-ics/output_gb/system/lib/modules/*
-make ARCH=arm CROSS_COMPILE=$COMPILER modules -j8
-make ARCH=arm CROSS_COMPILE=$COMPILER INSTALL_MOD_PATH=/home/root/kernel/dorimanx-ics/output_gb/system/lib/modules modules_install -j8
-cd /home/root/kernel/dorimanx-ics/output_gb/system/lib/modules
+
+mkdir -p $KERNELBASEDIR/
+rm -rf $KERNELBASEDIR/boot/*
+rm -rf $KERNELBASEDIR/system/lib/modules/*
+mkdir -p $KERNELBASEDIR/boot
+mkdir -p $KERNELBASEDIR/system/
+mkdir -p $KERNELBASEDIR/system/lib/
+mkdir -p $KERNELBASEDIR/system/lib/modules
+
+cp arch/arm/boot/zImage $KERNELBASEDIR/boot/zImage
+
+make ARCH=arm CROSS_COMPILE=$LOCAL_COMPILER INSTALL_MOD_PATH=$KERNELBASEDIR/system/lib/modules modules_install -j8
+
+cd $KERNELBASEDIR/system/lib/modules
 find -iname *.ko | xargs -i -t cp {} .
-rm -rf /home/root/kernel/dorimanx-ics/output_gb/system/lib/modules/lib
-stat /home/root/kernel/dorimanx-ics/output_gb/boot/zImage
-cd /home/root/kernel/dorimanx-ics/output_gb
-${COMPILER}strip --strip-debug system/lib/modules/*.ko
-zip -r Kernel_Dorimanx-HWA-V1.`date +"%H-%M-%d%m%y"`.zip .
+rm -rf $KERNELBASEDIR/system/lib/modules/lib
+stat $KERNELBASEDIR/boot/zImage
+cd ../../../
+zip -r tytung_HWA_kernel.`date +"%Y%m%d_%H_%M"`.zip boot system META-INF work nfo.prop
 else
 echo "Kernel STUCK in BUILD! no zImage exist"
 fi
